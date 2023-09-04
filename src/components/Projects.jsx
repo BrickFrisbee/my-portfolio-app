bbb8102c9aa0ad0d44daa563863f4f58acf6fde7
Ethan Hillman <erhillma@sundevils.asu.edu>
2023-09-04 14:50:15 -0700

    Flag added to Messenger grid item

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CanvasImg from "../assets/canvas_thumbnail.jpg";
 import PortfolioImg from "../assets/portfolio.PNG";
 import SignupImg from "../assets/Signup_Blank.PNG";
 import PowerShellImg from "../assets/powershell.png";
-import MessengerImg from "../assets/download.jpg"
-import SnifferImg from "../assets/sniffer.png"
-
+import MessengerImg from "../assets/download.jpg";
+import SnifferImg from "../assets/sniffer.png";
 
 const Projects = () => {
+  const [showFlag, setShowFlag] = useState(false);
+  const [flagOpacity, setFlagOpacity] = useState(1);
+
+  useEffect(() => {
+    if (showFlag) {
+      setFlagOpacity(1);
+      setTimeout(() => {
+        setFlagOpacity(0);
+      }, 4500);
+      {
+        /*begin fade out after 4.5 seconds*/
+      }
+    }
+  }, [showFlag]);
+
   return (
-    
     <div
       name="projects"
       className="w-full md:h-screen text-screen text-gray-300 bg-[#2a3439]"
     >
-      
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-cetner w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#9b00e8]">
             Projects
           </p>
           <p className="py-6 pb-0"># Check out some of my recent projects</p>
-          <p><i>- The Messaging Web Application is school project and is private to prevent plagiarism</i> </p>
+          <p>
+            <i>
+              - The Messaging Web Application is school project and is private
+              to prevent plagiarism
+            </i>{" "}
+          </p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-
           {/*Grid Item: */}
           <div
             style={{ backgroundImage: `url(${SignupImg})` }}
@@ -39,12 +55,7 @@ const Projects = () => {
               <div className="pt-8 text-center">
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    Project Repository
                   </button>
                 </a>
               </div>
@@ -64,12 +75,7 @@ const Projects = () => {
               <div className="pt-8 text-center">
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    Project Repository
                   </button>
                 </a>
               </div>
@@ -89,12 +95,7 @@ const Projects = () => {
               <div className="pt-8 text-center">
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    Project Repository
                   </button>
                 </a>
               </div>
@@ -114,12 +115,7 @@ const Projects = () => {
               <div className="pt-8 text-center">
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    Project Repository
                   </button>
                 </a>
               </div>
@@ -139,48 +135,69 @@ const Projects = () => {
               <div className="pt-8 text-center">
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    Project Repository
                   </button>
                 </a>
               </div>
             </div>
           </div>
 
-          {/*Grid Item: */}
+          {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${MessengerImg})` }}
-            className="shadow-lg shadow-[#040c16] group continer rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div relative"
           >
-            {/*Hover Effects: */}
+            {showFlag && <FlagTooltip opacity={flagOpacity} />}
+
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 .NET Messenger App
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                <a href="/" onClick={(e) => e.preventDefault()}>
+                  <button
+                    onClick={() => {
+                      setShowFlag(true);
+                      setTimeout(() => {
+                        setShowFlag(false);
+                      }, 5000);
+                    }}
+                    className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Project Repository
                   </button>
                 </a>
               </div>
             </div>
           </div>
-
-
         </div>
       </div>
     </div>
   );
 };
+
+function FlagTooltip({ opacity }) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "58px",
+        left: "50%",
+        transform: "translateX(-50%)",
+        backgroundColor: "#ff0000",
+        color: "white",
+        padding: "5px 10px",
+        borderRadius: "5px",
+        fontSize: "0.9rem",
+        whiteSpace: "nowrap",
+        opacity: opacity,
+        transition: "opacity 0.5s", 
+        fontWeight: "bold",
+      }}
+    >
+      School project - private to prevent plagarism. Please contact!
+    </div>
+  );
+}
 
 export default Projects;
